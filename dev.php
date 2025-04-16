b79fcdd8ec6e3422766d538e8df630e57b4ecaac
CamillePaillou <167968308+CamillePaillou@users.noreply.github.com>
2025-04-16 17:09:53 +0200

    page dev responsive

--- dev.php
+++ dev.php
@@ -38,17 +38,26 @@
         <div class="hero" data-id="1">
             <h1 class="overlay-name"><a href="/../camille.paillou/dev/hana.php">Hana Martinelli</a></h1>
             <p class="overlay-para">Site vitrine pour une sculptrice.</p>
-            <img src="images/dev/hana/hanam.PNG" alt="Image de fond">
+            <!--image vertical--> 
+            <img class="hero-img hero-img-vertical" src="images/dev/hana/hv.PNG" alt="Image de fond">
+            <!--image horizontal--> 
+            <img class="hero-img hero-img-horizontal" src="images/dev/hana/hanam.PNG" alt="Image de fond">
         </div>
         <div class="hero" data-id="2">
             <h1 class="overlay-name"><a href="/../camille.paillou/dev/spp.php">Son Papier Pixel</a></h1>
             <p class="overlay-para">Son Papier Pixel présente le workshop et la documentation issue du projet européen Codename Seedling. </p>
-            <img src="images/dev/spp/spp.png" alt="Image de fond">
+            <!--image vertical--> 
+            <img class="hero-img hero-img-vertical" src="images/dev/spp/sppv.PNG" alt="Image de fond">
+            <!--image horizontal--> 
+            <img class="hero-img hero-img-horizontal" src="images/dev/spp/spp.png" alt="Image de fond">
         </div>
         <div class="hero" data-id="3">
             <h1 class="overlay-name"><a href="/../camille.paillou/dev/bap.php">Bap.perf</a></h1>
             <p class="overlay-para">BAP.PERF est un site pour archiver les performances des étudiants des Beaux-Arts de Paris</p>
-            <img src="images/dev/bapperf/bap7.PNG" alt="Image de fond">
+            <!--image vertical--> 
+            <img class="hero-img hero-img-vertical" src="images/dev/bapperf/bapv1.PNG" alt="Image de fond">
+            <!--image horizontal--> 
+            <img class="hero-img hero-img-horizontal" src="images/dev/bapperf/bap7.PNG" alt="Image de fond">
         </div>
     </div>
 </div>
